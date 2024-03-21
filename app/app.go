19f6b05227fac1be4ba5950a45adc09b3585ea5f
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-21 17:34:26 -0300

    add: flags rearrangement and searchServer functionality add

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Generate() *cli.App {
 	app.Name = "Command line application"
 	app.Usage = "Search IPs and server names on the internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "duckduckgo.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IPs from internet addresses",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "duckduckgo.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IPs from internet addresses",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search the name of the server on the internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -40,5 +48,18 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, error := net.LookupNS(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
